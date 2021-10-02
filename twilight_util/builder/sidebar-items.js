c5acedd0127bbee377b3070e371edb49d6bc6d61
7596ff <7596ff@users.noreply.github.com>
2021-10-02 17:40:44 +0000

    deploy: 98e97de44be85d4ff019976cfc80e451e9e3055f

--- twilight_util/builder/sidebar-items.js
+++ twilight_util/builder/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["command","Create a [`Command`] with a builder."]]});
+initSidebarItems({"mod":[["command","Create a [`Command`] with a builder."]],"struct":[["CallbackDataBuilder","Create a [`CallbackData`] with a builder."]]});
